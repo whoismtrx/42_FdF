fe28935807020fe164532420541fd6df9e6e226e
MITIRIX <ghoufal1337@gmail.com>
2022-04-16 23:32:10 +0000

    FDF

--- fdf.c
+++ fdf.c
@@ -1,2 +1,11 @@
 #include "fdf.h"
 
+int main()
+{
+	void	*mlx;
+	void	*mlx_win;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "FdF");
+	mlx_loop(mlx);
+}
--- fdf.h
+++ fdf.h
@@ -4,5 +4,6 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <mlx.h>
 
 #endif
