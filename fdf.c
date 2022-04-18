77d48d4870c9cd03c1b7a278301247855da04cd8
MITIRIX <ghoufal1337@gmail.com>
2022-04-18 21:45:13 +0000

    FDF

--- fdf.c
+++ fdf.c
@@ -2,20 +2,14 @@
 
 int main()
 {
-	void	*mlx;
-	void	*img;
-	char	*add;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-	void	*mlx_win;
+	t_data	data;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "FdF");
-	img = mlx_new_image(mlx, 1920, 1080);
-	add = mlx_get_data_addr(img, &bits_per_pixel, &line_length, &endian);
-	mlx_pixel_put(mlx, mlx_win, 5, 5, 0X00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img, 0, 0);
-	mlx_loop(mlx);
 
+	data.mlx = mlx_init();
+	data.mlx_win = mlx_new_window(data.mlx, 1920, 1080, "FdF");
+	data.img = mlx_new_image(data.mlx, 1920, 1080);
+	data.add = mlx_get_data_addr(data.img, &data.bpp, &data.ll, &data.endian);
+	mlx_pixel_put(data.mlx, data.mlx_win, 5, 5, 0X00FF0000);
+	mlx_put_image_to_window(data.mlx, data.mlx_win, data.img, 0, 0);
+	mlx_loop(data.mlx);
 }
--- fdf.h
+++ fdf.h
@@ -6,4 +6,15 @@
 # include <stdio.h>
 # include <mlx.h>
 
+typedef struct s_data 
+{
+	void	*mlx;
+	void	*mlx_win;
+	void	*img;
+	char	*add;
+	int		bpp;
+	int		ll;
+	int		endian;
+}				t_data;
+
 #endif
