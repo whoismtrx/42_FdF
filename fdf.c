2d1566a6b61263076191cca3026d9927447f0b37
MITIRIX <ghoufal1337@gmail.com>
2022-04-19 23:26:43 +0000

    FDF

--- fdf.c
+++ fdf.c
@@ -1,5 +1,13 @@
 #include "fdf.h"
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->add + (y * data->ll + x * (data->bpp / 8));
+	*(unsigned int*)dst = color;
+}
+
 int main()
 {
 	t_data	data;
@@ -9,7 +17,7 @@ int main()
 	data.mlx_win = mlx_new_window(data.mlx, 1920, 1080, "FdF");
 	data.img = mlx_new_image(data.mlx, 1920, 1080);
 	data.add = mlx_get_data_addr(data.img, &data.bpp, &data.ll, &data.endian);
-	mlx_pixel_put(data.mlx, data.mlx_win, 5, 5, 0X00FF0000);
+	my_mlx_pixel_put(&data, 5, 5, 0X00FF0000);
 	mlx_put_image_to_window(data.mlx, data.mlx_win, data.img, 0, 0);
 	mlx_loop(data.mlx);
 }
