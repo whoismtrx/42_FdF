891aa29f36e69d99959fa822ad21f492d9eabcf9
MITIRIX <ghoufal1337@gmail.com>
2022-05-25 01:11:15 +0100

    FdF

--- fdf.c
+++ fdf.c
@@ -1,22 +1,165 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orekabe <orekabe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/23 02:44:55 by orekabe           #+#    #+#             */
+/*   Updated: 2022/05/25 01:10:58 by orekabe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fdf.h"
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	ft_get_map_size(t_data *data, char **argv)
 {
-	char	*dst;
+	int		fd;
+	char	*ret;
 
-	dst = data->add + (y * data->ll + x * (data->bpp / 8));
-	*(unsigned int*)dst = color;
+	fd = open(argv[1], O_RDONLY);
+	ret = get_next_line(fd);
+	while(ret)
+	{
+		data->size++;
+		ret = get_next_line(fd);	
+	}
 }
 
-int main()
+int	ft_get_line_size(char *line)
+{
+	int	size;
+	int	i;
+
+	size = 0;
+	i = 0;
+	while (line[i])
+	{
+		if (line[i] == ' ' && (line[i + 1] >= '0' && line[i + 1] <= '9'))
+			size++;
+		i++;
+	}
+	return(size + 1);
+}
+
+int	ft_get_z(t_data *data)
+{
+	static int	i;
+	static int	j;
+	static int	lj;
+	static int	b;
+	int			val;
+
+	j = 0;
+	lj = 0;
+	b = 1;
+	while (j < data->size)
+	{
+		if (j != lj)
+		{
+			lj = j;
+			i = 0;
+		}
+		while (data->map[j][i])
+		{
+			if (data->map[j][i] == ' ')
+			{
+				b = 1;
+				i++;
+			}
+			if ((data->map[j][i] >= '0' && data->map[j][i] <= '9') && b)
+			{
+				b = 0;
+				val = ft_atoi(&data->map[j][i]);
+				return (val);
+			}
+			i++;
+		}
+		j++;
+	}
+	return (0);	
+}
+
+void	ft_fill_map(t_data *data, char **argv)
+{
+	int		i;
+	int		j;
+	int		fd;
+	int		len;
+	char	*ret;
+	
+	fd = open(argv[1], O_RDONLY);
+	ret = get_next_line(fd);
+	j = 0;
+	while (ret)
+	{
+		i = 0;
+		len = ft_strlen(ret);
+		data->map[j] = (char *)malloc(sizeof(char) * len);
+		if (!data->map[i])
+			return ;
+		while (i < len)
+		{
+			data->map[j][i] = ret[i];
+			i++;
+		}
+		ret = get_next_line(fd);
+		j++;
+	}
+		
+}
+
+void	ft_fill_data(t_data *data)
+{
+	int		i;
+	int		j;
+	int		len;
+
+	j = 0;
+	while (j < data->size)
+	{
+		i = 0;
+		len = ft_get_line_size(data->map[j]);
+		data->z[j] = (int *)malloc(sizeof(int) * len);
+		if (!data->z)
+			return ;
+		data->c[j] = (int *)malloc(sizeof(int) * len);
+		if (!data->c)
+			return ;
+		while (i < len)
+		{
+			data->c[j][i] = ft_get_z(data);
+			i++;
+		}
+		j++;
+	}
+}
+
+void	ft_allocate_map(t_data *data, char **argv)
+{
+	ft_get_map_size(data, argv);
+	data->map = (char **)malloc(sizeof(char *) * data->size);
+	if (!data->map)
+		return ;
+	data->z = (int **)malloc(sizeof(int *) * data->size);
+	if (!data->z)
+		return ;
+	data->c = (int **)malloc(sizeof(int *) * data->size);
+	if (!data->c)
+		return ;
+	ft_fill_map(data, argv);
+	ft_fill_data(data);
+}
+
+int main(int argc, char **argv)
 {
 	t_data	data;
-
-	data.mlx = mlx_init();
-	data.mlx_win = mlx_new_window(data.mlx, 1920, 1080, "FdF");
-	data.img = mlx_new_image(data.mlx, 1920, 1080);
-	data.add = mlx_get_data_addr(data.img, &data.bpp, &data.ll, &data.endian);
-	my_mlx_pixel_put(&data, 5, 5, 0X00FF0000);
-	mlx_put_image_to_window(data.mlx, data.mlx_win, data.img, 0, 0);
-	mlx_loop(data.mlx);
+	
+	ft_allocate_map(&data, argv);
+	for(int i = 0; i < data.size; i++){
+		for(int j = 0; j < ft_get_line_size(data.map[i]); j++){
+			printf("%d",data.z[i][j]);	
+		}
+		printf("\n");
+	}
 }
--- fdf.h
+++ fdf.h
@@ -5,8 +5,9 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <mlx.h>
+# include <fcntl.h>
 
-typedef struct s_data 
+typedef struct s_graph
 {
 	void	*mlx;
 	void	*mlx_win;
@@ -15,6 +16,21 @@ typedef struct s_data
 	int		bpp;
 	int		ll;
 	int		endian;
+}				t_graph;
+
+typedef struct s_data
+{
+	char	**map;
+	int		**z;
+	int		**c;
+	int		size;
 }				t_data;
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+char	*ft_read(char *str, int fd);
+int		ft_strlen(const char *s);
+int		ft_atoi(const char *str);
 
 #endif
